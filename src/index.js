e6b79d3373e75864a74310ea9c3e5435a95968c3
Andrew-webdev <andreyrow96@gmail.com>
2020-03-17 18:38:40 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let leftBracketCount = 0;
+    let rightBracketCount = 0;
+
+    for (i = 0; i < str.length; i++) {
+        if (str[i] == '(') {
+            leftBracketCount++;
+        } else if (str[i] == ')') {
+            rightBracketCount++;
+        }
+    }
+    if (leftBracketCount == rightBracketCount) {
+        return true
+    } else return false
 }
